b0ea2c364e472bc75a1780853b9bddeb58ba13d9
7619646971 <119731279+7619646971@users.noreply.github.com>
2023-04-14 21:04:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
-// The EventSource is in this github 
-// https://github.com/mattboldt/typed.js
+/* ======================  toggle icon navbar  ====================== */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-// Typed is the Object
-
-const typed = new Typed(".multiple-text" , {
-    strings:["Frentend Developer","Backend Developer","QA Manuale Tester"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
-
-let menuIcon = document.querySelectorAll('section');
-let navbar = document.querySelectorAll('header nav a');
-
-menuIcon.onclik = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active')
-};
+    navbar.classList.toggle('active');
+}
 
-
-
-
-// ---scroll section ovtive link---
-
+/* ======================  Script Section Active link  ====================== */
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let navlinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+window.onscroll = () =>{
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links =>{
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
             });
         };
     });
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scroll >100);
 
-    //remove toggle icon and navbar when click navbar links 
+    /* ==========  Sticky navnbar  ============*/
+    let header = document.querySelector('header');
+    
+    header.classList.toggle('sticky', window.scrolly > 100);
+
+    /* ======================  remove toggle icon and navbar when click navbar link (scroll)  ====================== */
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
 };
 
-//scrollreveal
-ScrollReveal({
-    reset: true,
-    distance: '800px',
+ /* ==========  Scroll reveal ============*/
+
+ ScrollReveal({ 
+   // reset: true,
+    distance: '80px',
     duration: 2000,
     delay: 200
 });
 
-ScrollReveal().reveal('.home-content, .heading',{origin: 'top'});
-ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form',{origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img',{origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content',{origin: 'rigth'});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+ /* ==========  Typed Js ============*/
+ const typed = new Typed('.multiple-text', {
+    strings: ['Fullstack Developer', 'Fresher', 'programmer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+ });
